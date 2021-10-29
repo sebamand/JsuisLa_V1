20651ad84b424ffb3fcc65163f28dd8ef3bd4eac
seb <sebastien.amand@edu.esiee.fr>
2021-10-29 16:17:54 +0200

    Reconfiguraton des fragments

--- app/src/main/java/com/example/jsuisla_v1/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/jsuisla_v1/ui/home/HomeFragment.java
@@ -35,7 +35,7 @@ public class HomeFragment extends Fragment implements Serializable {
             }
         });
         //key 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
-        
+
         return root;
     }
 }
